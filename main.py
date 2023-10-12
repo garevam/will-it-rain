f9c661618df1d9a36e76c3fba6c7d7a8dcb83f1f
garevam <33831951+garevam@users.noreply.github.com>
2023-10-12 15:03:40 +0200

    Reinstalled openmeteo_py. I was unable to find WHY it wasn't working before, but marking its directory as a Sources Root fixed the issue, and now data can be succesfully retrieved.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import
-
+from openmeteo_py import OWmanager
+from openmeteo_py.Hourly.HourlyHistorical import HourlyHistorical
+from openmeteo_py.Daily.DailyHistorical import DailyHistorical
+from openmeteo_py.Options.HistoricalOptions import HistoricalOptions
+from openmeteo_py.Utils.constants import *
 
 """
 WIP
@@ -19,7 +22,22 @@ Made with Python 3.11
 
 
 def main():
+    # Latitude, Longitude
+    longitude = 33.89
+    latitude = -6.31
 
+    hourly = HourlyHistorical()
+    daily = DailyHistorical()
+    options = HistoricalOptions(latitude, longitude, nan, False, celsius, kmh, mm, iso8601, utc, "2022-12-31",
+                                "2023-02-26")
+
+    # notice that we had to give the value "None" for the hourly parameter,otherwise you'll be filling the hourly parameter instead of the daily one.
+    mgr = OWmanager(options, OWmanager.historical, hourly.all(), daily.all())
+
+    # Download data,here we want it as a key value json where the keys are dates and values the corresponding values of that date (technically timestamp)
+    meteo = mgr.get_data(1)
+
+    print(meteo)
 
 if __name__ == '__main__':
     main()
